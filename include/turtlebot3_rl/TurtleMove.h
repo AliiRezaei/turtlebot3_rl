668474030123b95dcb45fb1a7578ab1dc37caa8f
aliirezaei <alii.rezzaei@gmail.com>
2024-07-28 04:04:47 -0700

    add class body

--- include/turtlebot3_rl/TurtleMove.h
+++ include/turtlebot3_rl/TurtleMove.h
@@ -99,4 +99,297 @@ public:
   // get laser info in all directions :
   float *get_laser_full();
 };
+
+// TurtleBot3 constructor
+TurtleBot3::TurtleBot3() {
+  
+  // initializing node :
+  n = ros::NodeHandle("~");
+  
+  // laser topic and subscriber :
+  laser_topic = "/scan";
+  laser_sub = n.subscribe(laser_topic, 10, &TurtleBot3::laser_callback, this);
+  
+  // velocity command topic and publisher :
+  vel_topic = "/cmd_vel";
+  vel_pub = n.advertise<geometry_msgs::Twist>(n.resolveName(vel_topic), 1);
+  
+  // odometry topic and subscriber :
+  odom_topic = "/odom";
+  odom_sub = n.subscribe(odom_topic, 10, &TurtleBot3::odom_callback, this);
+  
+  // log init info :
+  // ROS_INFO("Initializing node .................................");
+  usleep(1000000);
+  ros::spinOnce();
+}
+
+void TurtleBot3::laser_callback( const sensor_msgs::LaserScan::ConstPtr &laser_msg) {
+  laser_range = laser_msg->ranges;
+  // ROS_INFO("Laser value: %f", laser_range);
+}
+void TurtleBot3::odom_callback(const nav_msgs::Odometry::ConstPtr &odom_msg) {
+  
+  // turtlebot3 position along x axis :
+  x_pos = odom_msg->pose.pose.position.x;
+  
+  // turtlebot3 position along y axis :
+  y_pos = odom_msg->pose.pose.position.y;
+  
+  // turtlebot3 position along z axis :
+  z_pos = odom_msg->pose.pose.position.z;
+  
+  // turtlebot3 quaternion x_quat:
+  x_quat = odom_msg->pose.pose.orientation.x;
+
+  // turtlebot3 quaternion y_quat:
+  y_quat = odom_msg->pose.pose.orientation.y;
+  
+  // turtlebot3 quaternion z_quat:
+  z_quat = odom_msg->pose.pose.orientation.z;
+  
+  // turtlebot3 quaternion y_quat:
+  w_quat = odom_msg->pose.pose.orientation.w;
+
+  // quat to rpy :
+  Quaternion2RollPitchYaw();
+
+}
+
+void TurtleBot3::Quaternion2RollPitchYaw() {
+
+  this->roll  = atan2(2.0*(y_quat*z_quat + w_quat*x_quat), w_quat*w_quat - x_quat*x_quat - y_quat*y_quat + z_quat*z_quat);
+  this->pitch = asin(-2.0*(x_quat*z_quat - w_quat*y_quat));
+  this->yaw   = atan2(2.0*(x_quat*y_quat + w_quat*z_quat), w_quat*w_quat + x_quat*x_quat - y_quat*y_quat - z_quat*z_quat);
+
+}
+void TurtleBot3::move() {
+  // Rate of publishing
+  ros::Rate rate(10);
+
+  ros::Time start_time = ros::Time::now();
+  ros::Duration timeout(2.0); // Timeout of 2 seconds
+  while (ros::Time::now() - start_time < timeout) {
+    ros::spinOnce();
+    vel_msg.linear.x = +0.5;
+    vel_msg.angular.z = 0.0;
+    vel_pub.publish(vel_msg);
+    rate.sleep();
+  }
+  vel_msg.linear.x = 0.0;
+  vel_msg.angular.z = 0.0;
+  vel_pub.publish(vel_msg);
+}
+
+void TurtleBot3::move_forward(int time) {
+  
+  // rate of publishing :
+  int freq = 10;
+  ros::Rate rate(freq);
+
+  // start time and duration :
+  ros::Time start_time = ros::Time::now();
+  ros::Duration timeout(time);
+
+  // go forward for "time" seconds :
+  while (ros::Time::now() - start_time < timeout) {
+    // ROS_INFO_STREAM("Moving forward ........... ");
+    ros::spinOnce();
+    
+    // adjust velocity and publish them :
+    vel_msg.linear.x = 0.2;
+    vel_msg.linear.y = 0.0;
+    vel_msg.angular.z = 0.0;
+    vel_pub.publish(vel_msg);
+
+    // waiting :
+    rate.sleep();
+  }
+
+  // publish zero velocity command :
+  vel_msg.linear.x = 0.0;
+  vel_msg.angular.z = 0.0;
+  vel_pub.publish(vel_msg);
+} // end of move_forward
+
+void TurtleBot3::move_forward_meters(float meters) 
+{
+  // ROS_INFO_STREAM("Robot has started from x = " << x_pos);
+
+  // rate of publishing :
+  ros::Rate rate(10);
+  ros::Time start_time = ros::Time::now();
+
+  // target position and stop criteria :
+  float x_target = x_pos + meters;
+  double eps = 0.0001;
+
+  // go forward until reaching target position :
+  while (x_target - x_pos > eps) {
+    // ROS_INFO_STREAM("Moving forward " << meters << " meters");
+    ros::spinOnce();
+    vel_msg.linear.x = 0.2;
+    // vel_msg.linear.x = 0.3 * (x_target - x_pos);
+    vel_msg.linear.y = 0.0;
+    vel_msg.angular.z = 0.0;
+    vel_pub.publish(vel_msg);
+    rate.sleep();
+  }
+
+  // stop moving :
+  vel_msg.linear.x = 0.0;
+  vel_msg.angular.z = 0.0;
+  vel_pub.publish(vel_msg);
+  
+  // print robot moving status :
+  ros::Time end_time = ros::Time::now();
+  // ROS_INFO_STREAM("Robot reached " << x_pos << " after " << end_time - start_time << " seconds");
+}
+
+void TurtleBot3::move_backward(int time) {
+  // Rate of publishing
+  ros::Rate rate(10);
+
+  ros::Time start_time = ros::Time::now();
+  ros::Duration timeout(time);
+  while (ros::Time::now() - start_time < timeout) {
+    // ROS_INFO_STREAM("Moving backwards ........... ");
+    ros::spinOnce();
+    vel_msg.linear.x = -0.2;
+    vel_msg.linear.y = 0.0;
+    vel_msg.angular.z = 0.0;
+    vel_pub.publish(vel_msg);
+    rate.sleep();
+  }
+  vel_msg.linear.x = 0.0;
+  vel_msg.angular.z = 0.0;
+  vel_pub.publish(vel_msg);
+}
+
+void TurtleBot3::move_backward_meters(float meters) 
+{
+  // ROS_INFO_STREAM("Robot has started from x = " << x_pos);
+
+  // rate of publishing :
+  ros::Rate rate(10);
+  ros::Time start_time = ros::Time::now();
+
+  // target position and stop criteria :
+  float x_target = x_pos - meters;
+  double eps = 0.0001;
+
+  // go forward until reaching target position :
+  while (x_pos - x_target > eps) {
+    // ROS_INFO_STREAM("Moving forward " << meters << " meters");
+    ros::spinOnce();
+    vel_msg.linear.x = -0.2;
+    // vel_msg.linear.x = -0.3 * (x_pos - x_target);
+    vel_msg.linear.y = 0.0;
+    vel_msg.angular.z = 0.0;
+    vel_pub.publish(vel_msg);
+    rate.sleep();
+  }
+
+  // stop moving :
+  vel_msg.linear.x = 0.0;
+  vel_msg.angular.z = 0.0;
+  vel_pub.publish(vel_msg);
+  
+  // print robot moving status :
+  ros::Time end_time = ros::Time::now();
+  // ROS_INFO_STREAM("Robot reached " << x_pos << " after " << end_time - start_time << " seconds");
+}
+
+void TurtleBot3::turn(string clock, int n_secs) {
+  ros::Rate rate(10);
+  ros::Time start_time = ros::Time::now();
+  ros::Duration timeout(n_secs);
+
+  double WZ = 0.0;
+  if (clock == "clockwise") {
+    // ROS_INFO_STREAM("Turning clockwise..............");
+    WZ = -2.5;
+  } else if (clock == "counterclockwise") {
+    // ROS_INFO_STREAM("Turning counterclockwise ........... ");
+    WZ = 2.5;
+  }
+
+  while (ros::Time::now() - start_time < timeout) {
+    ros::spinOnce();
+    vel_msg.linear.x = 0.0;
+    vel_msg.angular.z = WZ;
+    vel_pub.publish(vel_msg);
+    rate.sleep();
+  }
+  vel_msg.linear.x = 0.0;
+  vel_msg.angular.z = 0.0;
+  vel_pub.publish(vel_msg);
+}
+
+void TurtleBot3::turn_degree(float deg)
+{
+  // Rate of publishing
+  ros::Rate rate(10);
+
+  double theta_target = deg * 3.14159265 / 180.0 + yaw;
+
+  int sign_theta = 0;
+  if(deg > 0.0) {sign_theta =  1;}
+  if(deg < 0.0) {sign_theta = -1;}
+
+  double eps = 0.0001;
+  while(sign_theta * (theta_target - yaw) > eps)
+  {
+    ros::spinOnce();
+    vel_msg.linear.x = 0.0;
+    vel_msg.linear.y = 0.0;
+    vel_msg.angular.z = sign_theta * 0.3;
+    vel_pub.publish(vel_msg);
+    // ROS_INFO_STREAM("Turning " << deg << " degrees");
+    rate.sleep(); 
+  }
+  vel_msg.linear.x = 0.0;
+  vel_msg.linear.y = 0.0;
+  vel_msg.angular.z = 0.0;
+  vel_pub.publish(vel_msg);
+}
+
+void TurtleBot3::stop_moving() {
+  // ROS_INFO_STREAM("Stopping the robot ........... ");
+  vel_msg.linear.x = 0.0;
+  vel_msg.linear.y = 0.0;
+  vel_msg.angular.z = 0.0;
+  vel_pub.publish(vel_msg);
+}
+
+float TurtleBot3::get_position(int param) {
+  if (param == 1) {
+    return this->x_pos;
+  } else if (param == 2) {
+    return this->y_pos;
+  } else if (param == 3) {
+    return this->z_pos;
+  }
+  return 0;
+}
+
+list<float> TurtleBot3::get_position_full() {
+  list<float> coordinates({this->x_pos, this->y_pos, this->z_pos});
+  return coordinates;
+}
+
+double TurtleBot3::get_time() {
+  double secs = ros::Time::now().toSec();
+  return secs;
+}
+
+float TurtleBot3::get_laser(int index) {
+  return laser_range[index];
+}
+
+float *TurtleBot3::get_laser_full() {
+  float *laser_range_ptr = laser_range.data();
+  return laser_range_ptr;
+}
+
 #endif
