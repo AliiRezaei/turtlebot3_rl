59b6de8ccf549bd0a34c5492bc81940e1f5b4b3c
aliirezaei <alii.rezzaei@gmail.com>
2024-07-29 04:33:45 -0700

    create states done

--- include/turtlebot3_rl/ReinforcementLearning.h
+++ include/turtlebot3_rl/ReinforcementLearning.h
@@ -15,7 +15,11 @@ class QLearning {
 
     randomGen randGenerator;
 
-    void   set_actions();
+    float x_space_precise;
+    float y_space_precise;
+    float theta_space_precise;
+
+    
     
   public:
 
@@ -24,6 +28,9 @@ class QLearning {
     int   *Actions = new int[5];
     
     QLearning(int n_actions, float epsilon);
+
+    void   create_actions();
+    std::vector<std::vector<float>>   create_states(stateInfo x, stateInfo y, stateInfo theta);
     int    get_random_action(int *actions_set);
     int    get_best_action(float *action_values, int *actions_set);
     int    epsilon_greedy(double reward[], int *actions_set, double actions_count[]);
@@ -45,13 +52,64 @@ QLearning::QLearning(int n_actions, float epsilon) {
     maxFind maxInfo;
     this->maxInfo = maxInfo;
     
-    set_actions();
+    create_actions();
+    // create_states();
 }
 
-void QLearning::set_actions() {
+void QLearning::create_actions() {
     for(int i=0; i<n_actions; i++) {
         *(Actions + i) = i;
     }
+}
+
+std::vector<std::vector<float>> QLearning::create_states(stateInfo x, stateInfo y, stateInfo theta) {
+  // extract info :
+  float x_min = x.min;
+  float x_max = x.max;
+  int   x_n   = x.n;
+  this->x_space_precise = (x_max - x_min) / (x_n - 1.0);
+
+  float y_min = y.min;
+  float y_max = y.max;
+  int   y_n   = y.n;
+  this->y_space_precise = (y_max - y_min) / (y_n - 1.0);
+
+  float theta_min = theta.min;
+  float theta_max = theta.max;
+  int   theta_n   = theta.n;
+  this->theta_space_precise = (theta_max - theta_min) / (theta_n - 1.0);
+
+  // x space :
+  float *x_ = linspace<float>(x_min, x_max, x_n);
+  // this->x_space = x_;
+  
+  // y space :
+  float *y_ = linspace<float>(y_min, y_max, y_n);
+  // this->y_space = y_;
+
+  // theta space :
+  float *theta_ = linspace<float>(theta_min, theta_max, theta_n);
+  // this->theta_space = theta_;
+
+  // create augmented states matrix :
+  std::size_t all_states_row = x_n * y_n * theta_n;
+  std::size_t all_states_col = 3;
+
+  std::vector<std::vector<float>> augmented_states(all_states_row, std::vector<float>(all_states_col));
+  for(std::size_t i = 0; i < x_n; i++) {
+    for(std::size_t j = 0; j < y_n; j++) {
+      for(std::size_t k = 0; k < theta_n; k++) {
+        std::size_t index = i * (y_n * theta_n) + j * theta_n + k;
+        augmented_states[index][0] = x_[i];
+        augmented_states[index][1] = y_[j];
+        augmented_states[index][2] = theta_[k];
+      }
+    }
+  }
+  delete[] x_;
+  delete[] y_;
+  delete[] theta_;
+  return augmented_states;
 }
 
 int QLearning::get_random_action(int *actions_set) {
