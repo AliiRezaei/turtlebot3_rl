a004b959229555f88ed8609cc7ce88420f7caec5
aliirezaei <alii.rezzaei@gmail.com>
2024-07-31 01:37:21 -0700

    add go_target

--- include/turtlebot3_rl/TurtleMove.h
+++ include/turtlebot3_rl/TurtleMove.h
@@ -8,7 +8,12 @@
 #include "nav_msgs/Odometry.h"
 #include <string.h>
 #include <math.h>
+#include <turtlebot3_rl/Toolbox.h>
+
+
 using namespace std;
+
+
 
 class TurtleBot3 {
 private:
@@ -80,6 +85,9 @@ public:
 
   // turn arbit degrees :
   void turn_degree(float deg);
+
+  // go to the target pose :
+  void go_target(float *current_pose, float *target_pose);
 
   // stop moving :
   void stop_moving();
@@ -345,43 +353,31 @@ void TurtleBot3::turn(string clock, int n_secs) {
   vel_pub.publish(vel_msg);
 }
 
-// void TurtleBot3::turn_degree(float deg)
-// {
-//   // Rate of publishing
-//   ros::Rate rate(10);
-
-//   double theta_target = deg * 3.14159265 / 180.0 + yaw;
-
-//   int sign_theta = 0;
-//   if(deg > 0.0) {sign_theta =  1;}
-//   if(deg < 0.0) {sign_theta = -1;}
-
-//   double eps = 0.0001;
-//   while(sign_theta * (theta_target - yaw) > eps)
-//   {
-//     ros::spinOnce();
-//     vel_msg.linear.x = 0.0;
-//     vel_msg.linear.y = 0.0;
-//     vel_msg.angular.z = sign_theta * 0.3;
-//     vel_pub.publish(vel_msg);
-//     // ROS_INFO_STREAM("Turning " << deg << " degrees");
-//     rate.sleep(); 
-//   }
-//   vel_msg.linear.x = 0.0;
-//   vel_msg.linear.y = 0.0;
-//   vel_msg.angular.z = 0.0;
-//   vel_pub.publish(vel_msg);
-// }
-
-
 void TurtleBot3::turn_degree(float deg)
 {
+  // Rate of publishing
+  ros::Rate rate(10);
+
+  double theta_target = deg * 3.14159265 / 180.0 + yaw;
+
   int sign_theta = 0;
   if(deg > 0.0) {sign_theta =  1;}
   if(deg < 0.0) {sign_theta = -1;}
-  vel_msg.linear.x  = 0.06;
-  vel_msg.linear.y  = 0.0;
-  vel_msg.angular.z = sign_theta * 0.04;
+
+  double eps = 0.0001;
+  while(sign_theta * (theta_target - yaw) > eps)
+  {
+    ros::spinOnce();
+    vel_msg.linear.x = 0.0;
+    vel_msg.linear.y = 0.0;
+    vel_msg.angular.z = sign_theta * 0.3;
+    vel_pub.publish(vel_msg);
+    // ROS_INFO_STREAM("Turning " << deg << " degrees");
+    rate.sleep(); 
+  }
+  vel_msg.linear.x = 0.0;
+  vel_msg.linear.y = 0.0;
+  vel_msg.angular.z = 0.0;
   vel_pub.publish(vel_msg);
 }
 
@@ -392,6 +388,84 @@ void TurtleBot3::stop_moving() {
   vel_msg.linear.y = 0.0;
   vel_msg.angular.z = 0.0;
   vel_pub.publish(vel_msg);
+}
+
+void TurtleBot3::go_target(float *current_pose, float *target_pose) {
+
+
+  // extract target_pose :
+  float target_x     = *(target_pose + 0);
+  float target_y     = *(target_pose + 1);
+  float target_theta = *(target_pose + 2);
+
+  // extract current_pose :
+  float current_x     = *(current_pose + 0);
+  float current_y     = *(current_pose + 1);
+  float current_theta = *(current_pose + 2);
+
+  // normalize target_theta :
+  if(target_theta > _PI_NUMBER_) {target_theta -= 2.0 * _PI_NUMBER_;}
+
+  // init loop params :
+  float distance;   // distance between actual position and target position
+  float lambda;     // distance between actual orientation and target orientation 
+  float alpha;      // auxiliary var
+  float beta;       // auxiliary var
+  float v;          // linear  vel 
+  float w;          // angular vel
+  float v_scal;     // scaled linear  vel 
+  float w_scal;     // scaled angular vel
+
+  // gains :
+    float K_distannce =   2.0;
+    float K_alpha     =  15.0;
+    float K_beta      = - 3.0;
+    float V_const     =   0.1;
+
+  // reaching precise :
+  float eps = 0.0001;
+
+  // reaching status :
+  bool robot_in_target = false;
+
+  // main loop :
+  while(!robot_in_target) {
+
+    // update distance :
+    distance = eucliden_distance(current_pose, target_pose, 2);
+
+    // update lambda :
+    lambda = atan2(target_y - current_y, target_x - current_x);
+
+    // update alpha and beta :
+    alpha = (lambda -  current_theta + pi) % (2 * _PI_NUMBER_) - _PI_NUMBER_;
+    beta  = (target_theta - lambda + pi) % (2 * _PI_NUMBER_) - _PI_NUMBER_;
+
+    // check reaching status :
+    if(distance < eps && abs(current_theta - target_theta) < eps) {
+      vel_msg.linear.x  = 0.0;
+      vel_msg.linear.y  = 0.0;
+      vel_msg.angular.z = 0.0;
+      vel_pub.publish(vel_msg);
+      robot_in_target = true;
+    }
+    else {
+      float v = K_distannce * distance;
+      float w = K_alpha * alpha + K_beta * beta;
+      float v_scal = v / abs(v) * V_const;
+      float w_scal = w / abs(v) * V_const;
+
+      vel_msg.linear.x  = v_scal;
+      vel_msg.linear.y  = 0.0;
+      vel_msg.angular.z = w_scal;
+      vel_pub.publish(vel_msg);
+    }
+
+    // update pose :
+    *(current_pose + 0) = this->x_pos;
+    *(current_pose + 1) = this->y_pos;
+    *(current_pose + 2) = this->yaw;
+  }
 }
 
 float TurtleBot3::get_position(int param) {
